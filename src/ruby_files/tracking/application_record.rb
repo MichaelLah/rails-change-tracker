19c689c2dd7dc8e171799e6b299e858707ae9493
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2020-02-22 14:47:42 -0500

    error handling

--- src/ruby_files/tracking/application_record.rb
+++ src/ruby_files/tracking/application_record.rb
@@ -1,12 +1,20 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   after_update do |model|
-    ActiveRecord::Base.connection.execute("INSERT INTO tracking (table_name, action, object_id, changes) VALUES ('#{model.class.table_name}', 'UPDATE','#{model.id}', '#{model.saved_changes.to_json.gsub("'", %q(\\\')).gsub('\n','')}')")
+    add_entry(model.class.table_name, 'CREATE', model.id, model.saved_changes.to_json.gsub("'", %q(\\\')).gsub('\n',''))
   end
   after_create do |model|
-    ActiveRecord::Base.connection.execute("INSERT INTO tracking (table_name, action, object_id, changes) VALUES ('#{model.class.table_name}', 'CREATE','#{model.id}', '#{model.to_json.gsub("'", %q(\\\')).gsub('\n','')}')")
+    add_entry(model.class.table_name, 'CREATE', model.id, model.to_json.gsub("'", %q(\\\')).gsub('\n',''))
   end
   after_destroy do |model|
-    ActiveRecord::Base.connection.execute("INSERT INTO tracking (table_name, action, object_id, changes) VALUES ('#{model.class.table_name}', 'DESTROY','#{model.id}', '#{model.to_json.gsub("'", %q(\\\')).gsub('\n','')}')")
+    add_entry(model.class.table_name, 'DESTROY', model.id, model.to_json.gsub("'", %q(\\\')).gsub('\n',''))
+  end
+
+  def add_entry(model_name, action, model_id, changes)
+    begin
+      ActiveRecord::Base.connection.execute("INSERT INTO tracking (table_name, action, object_id, changes) VALUES ('#{model_name}', '#{action}' ,'#{model_id}', '#{changes}')")
+    rescue
+      ActiveRecord::Base.connection.execute("INSERT INTO tracking (table_name, action, object_id, changes) VALUES ('#{model_name}', '#{action}' ,'#{model_id}', '#{"ERROR PARSING".to_json}')")
+    end
   end
 end
