81f5bc979bb2527bce3141cc997fb7cc7a4e17cd
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2020-02-22 14:05:34 -0500

    add init

--- src/rails-tracker.sh
+++ src/rails-tracker.sh
@@ -8,8 +8,14 @@ mysql_prefix="mysql -h 0.0.0.0 -P 3306 -u root -e"
 tracking_table="tracking"
 currnet_db=$(head -n 1 $beam_api_dir/.development_mysql_override)
 
+rails_path_file=config/rails_path.txt
+tracker_path_file=config/tracker_path.txt
+
 print_usage() {
-  echo "lol git gud"
+  echo "Usage:"
+  echo "rails-tracker.sh init - setup the tracker for use"
+  echo "rails-tracker.sh start - Create a tracking table and start tracking all active record changes"
+  echo "rails-tracker.sh stop - Drop tracking table and stop tracking changes"
 }
 
 start_tracking() {
@@ -29,6 +35,25 @@ reset_tracking() {
   echo "clearning tracking table."
   mysql -h 0.0.0.0 -P 3306 -u root -e "TRUNCATE TABLE $currnet_db.$tracking_table"
 }
+
+init() {
+  # setup beam-api location
+  if [ ! -f "$rails_path_file" ]; then
+    touch "$rails_path_file"
+  fi 
+  echo "Enter path of your beam-api folder\n"
+  read response
+  echo $response > $rails_path_file
+
+  # setup tracker location
+  if [ ! -f "$tracker_path_file" ]; then
+    touch "$tracker_path_file"
+  fi 
+  echo "Enter path of your tracker folder\n"
+  read response
+  echo $response > $tracker_path_file
+  echo "finished! Try: \n\n ./rails-tracker.sh start\n\nTo start tracking active record changes."
+}
 #echo $currnet_db
 #stop_tracking
 #start_tracking
@@ -43,6 +68,9 @@ case $1 in
   ;;
 "reset")
   reset_tracking
+  ;;
+"init")
+  init
   ;;
 *)
   print_usage
